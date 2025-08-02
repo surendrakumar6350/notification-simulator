2b5b81c5a25e179bcd02cab8100b930a3595a9ca
Admin <kamleshksks456@gmail.com>
2025-08-02 18:27:08 +0000

    update: Ads Removed

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,30 +26,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-
-        <Script src="/popup.js" strategy="afterInteractive" />
-
-
-        <Script
-          src="//madurird.com/tag.min.js"
-          strategy="afterInteractive"
-          data-zone="9636850"
-          data-cfasync="false"
-        />
-
-        {/* Additional dynamic ad script (groleegni.net) */}
-        <Script id="groleegni-script" strategy="afterInteractive">
-          {`
-            (function(d,z,s){
-              s.src='https://'+d+'/401/'+z;
-              try {
-                (document.body || document.documentElement).appendChild(s);
-              } catch(e) {}
-            })('groleegni.net',9636911,document.createElement('script'));
-          `}
-        </Script>
-
-
       </body>
     </html>
   );
